4ad335d64685d1cfbf7bfffe93a4c42ab633f6d3
Lucas Von Ah <lucas_vonah@hotmail.com>
2023-11-23 22:40:06 -0300

    Add Discriminators

--- LucasFootLEntities/Competitions/Competition.cs
+++ LucasFootLEntities/Competitions/Competition.cs
@@ -10,6 +10,7 @@ public abstract class Competition
     public abstract CompetitionType Type { get; }
     public abstract CompetitionFormat Format { get; }
     public abstract int NumberOfTeams { get; }
+    public abstract string Discriminator { get; }
 }
 
 public abstract class NationalCompetition : Competition
@@ -17,6 +18,7 @@ public abstract class NationalCompetition : Competition
     public static LeagueFormat LeagueFormat => LeagueFormat.Double;
     public override CompetitionType Type => CompetitionType.National;
     public override CompetitionFormat Format => CompetitionFormat.League;
+    public override string Discriminator => "National";
     public abstract LeagueDivision Division { get; }
     public abstract int Relegated { get; }
     public abstract int Promoted { get; }
@@ -25,6 +27,7 @@ public abstract class NationalCompetition : Competition
 
 public abstract class MainNationalCompetition : NationalCompetition
 {
+    public override string Discriminator => "MainNational";
     public abstract int MainClassifiedDirectly { get; }
     public abstract int MainClassifiedByPlayoff { get; }
     public abstract int SecondaryClassified { get; }
@@ -32,6 +35,7 @@ public abstract class MainNationalCompetition : NationalCompetition
 
 public abstract class CupCompetition : Competition
 {
+    public override string Discriminator => "Cup";
     public override CompetitionType Type => CompetitionType.National;
     public override CompetitionFormat Format => CompetitionFormat.Knockout;
     public abstract KnockoutFormat KnockoutFormat { get; }
@@ -40,6 +44,7 @@ public abstract class CupCompetition : Competition
 
 public abstract class ContinentalCompetition : Competition
 {
+    public override string Discriminator => "Continental";
     public static GroupFormat GroupFormat => GroupFormat.Double;
     public static KnockoutFormat KnockoutFormat => KnockoutFormat.Double;
     public override CompetitionType Type => CompetitionType.Continental;
